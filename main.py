bc6da07069e8609052ab695453ae02c6ed7bbf63
Sumit Kumar <120319966+sumitkumardev@users.noreply.github.com>
2025-05-10 12:28:15 +0000

    minor changes

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import os
 
 app = Flask(__name__)
 
+# Get MongoDB URI from environment variable
 MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
-db = client["eduter"]
-collection = db["users"]
 
-@app.route("/api/users")
-def get_users():
-    users = list(collection.find({}, {"_id": 0}))
-    return jsonify(users)
+# Connect to MongoDB
+client = MongoClient(MONGO_URI)
+db = client["newsque"]
+collection = db["newsque_resource"]
+
+@app.route("/api/resources")
+def get_resources():
+    resources = list(collection.find({}, {"_id": 0}))  # Don't return _id
+    return jsonify(resources)
 
 if __name__ == "__main__":
     app.run()
